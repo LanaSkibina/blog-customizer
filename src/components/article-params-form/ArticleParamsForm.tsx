6ba6136f237bdb79d1dac9ddfd1193655a585092
LanaSkibina <127964337+LanaSkibina@users.noreply.github.com>
2024-07-22 03:05:40 +0300

    ПР10

--- src/components/article-params-form/ArticleParamsForm.tsx
+++ src/components/article-params-form/ArticleParamsForm.tsx
@@ -1,16 +1,139 @@
-import { ArrowButton } from 'components/arrow-button';
-import { Button } from 'components/button';
+// Импорт компонентов React
+import { FormEvent, useRef, useState } from 'react';
+import clsx from 'clsx';
 
+// Импорт стилей
 import styles from './ArticleParamsForm.module.scss';
 
-export const ArticleParamsForm = () => {
+// Импорт компонентов приложения
+import { ArrowButton } from 'components/arrow-button';
+import { Button } from 'components/button';
+import { Text } from 'components/text';
+import { Select } from 'components/select';
+
+import {
+	OptionType,
+	backgroundColors,
+	contentWidthArr,
+	defaultArticleState,
+	fontColors,
+	fontFamilyOptions,
+	fontSizeOptions,
+} from 'src/constants/articleProps';
+import { RadioGroup } from '../radio-group';
+import { Separator } from '../separator';
+import { useClose } from './hooks/useCloseForm';
+
+type ArticleParamsFormProps = {
+	state: typeof defaultArticleState;
+	setState: React.Dispatch<React.SetStateAction<typeof defaultArticleState>>;
+	resetStyles: () => void; // сброс параметров формы до дефолтных
+	applyStyles: () => void; // применить выбранные параметры формы
+};
+
+export const ArticleParamsForm = ({
+	state,
+	setState,
+	resetStyles,
+	applyStyles,
+}: ArticleParamsFormProps) => {
+	const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+	const toggleMenu = () => {
+		setIsMenuOpen((prevOpen) => !prevOpen);
+	};
+
+    // обработчик изменения названия шрифта
+	const setFontFamily = (value: OptionType) => {
+		setState({ ...state, fontFamilyOption: value });
+	};
+    
+	// обработчик изменения размера шрифта
+	const setFontSize = (value: OptionType) => {
+		setState({ ...state, fontSizeOption: value });
+	};
+    
+	// обработчик изменения цвета шрифта
+	const setFontColor = (value: OptionType) => {
+		setState({ ...state, fontColor: value });
+	};
+    
+	// обработчик изменения цвета фона
+	const setBackgroundColor = (value: OptionType) => {
+		setState({ ...state, backgroundColor: value });
+	};
+    
+	// обработчик изменения ширины отображения контента
+	const setContentWidth = (value: OptionType) => {
+		setState({ ...state, contentWidth: value });
+	};
+
+	const formRef = useRef<HTMLFormElement | null>(null);
+
+	useClose({
+		isOpen: isMenuOpen,
+		onClose: toggleMenu,
+		rootRef: formRef,
+	});
+    // Сборка формы
 	return (
 		<>
-			<ArrowButton />
-			<aside className={styles.container}>
-				<form className={styles.form}>
+			<ArrowButton
+				onClick={() => !isMenuOpen && toggleMenu()}
+				isOpen={isMenuOpen}
+			/>
+			<aside
+				className={clsx(styles.container, {
+					[styles.container_open]: isMenuOpen,
+				})}>
+				<form
+					className={styles.form}
+					ref={formRef}
+					onSubmit={(e: FormEvent) => {
+						e.preventDefault();
+						applyStyles();
+					}}>
+					<Text as={'h2'} size={31} weight={800} uppercase={true}>
+						Задайте параметры
+					</Text>
+					<Select
+						selected={state.fontFamilyOption}
+						options={fontFamilyOptions}
+						placeholder='Выберите шрифт'
+						title='шрифт'
+						onChange={setFontFamily}
+					/>
+					<RadioGroup
+						name='fontSize'
+						options={fontSizeOptions}
+						selected={state.fontSizeOption}
+						title='размер шрифта'
+						onChange={setFontSize}
+					/>
+					<Select
+						selected={state.fontColor}
+						options={fontColors}
+						placeholder='Выберите цвет'
+						title='цвет шрифта'
+						onChange={setFontColor}
+					/>
+					<Separator />
+					<Select
+						selected={state.backgroundColor}
+						options={backgroundColors}
+						placeholder='Выберите цвет'
+						title='цвет фона'
+						onChange={setBackgroundColor}
+					/>
+					<Select
+						selected={state.contentWidth}
+						options={contentWidthArr}
+						placeholder='Выберите ширину'
+						title='ширина контента'
+						onChange={setContentWidth}
+					/>
 					<div className={styles.bottomContainer}>
-						<Button title='Сбросить' type='reset' />
+						<Button title='Сбросить' type='reset' onClick={resetStyles} />
 						<Button title='Применить' type='submit' />
 					</div>
 				</form>
--- src/index.tsx
+++ src/index.tsx
@@ -1,36 +1,62 @@
+// Импорт компонентов React
 import { createRoot } from 'react-dom/client';
-import { StrictMode, CSSProperties } from 'react';
+import { StrictMode, CSSProperties, useState } from 'react';
 import clsx from 'clsx';
 
+// Импорт компонентов приложения
 import { Article } from './components/article/Article';
 import { ArticleParamsForm } from './components/article-params-form/ArticleParamsForm';
 import { defaultArticleState } from './constants/articleProps';
 
+// Импорт стилей
 import './styles/index.scss';
 import styles from './styles/index.module.scss';
 
+// Получаем корневой dom-узел и создаем корень React приложения
 const domNode = document.getElementById('root') as HTMLDivElement;
 const root = createRoot(domNode);
 
+
 const App = () => {
+	const [articleState, setArticleState] = useState(defaultArticleState);
+	const [formState, setFormState] = useState(defaultArticleState);
+    
+	// для дефлотных
+	const resetFormState = () => {
+		setFormState(defaultArticleState);
+		setArticleState(defaultArticleState);
+	};
+    
+	// для кастомных настроек
+	const applyState = () => {
+		setArticleState(formState);
+	};
+    
+	// Отрисовка компонентов приложения
 	return (
-		<div
+		<main
 			className={clsx(styles.main)}
 			style={
 				{
-					'--font-family': defaultArticleState.fontFamilyOption.value,
-					'--font-size': defaultArticleState.fontSizeOption.value,
-					'--font-color': defaultArticleState.fontColor.value,
-					'--container-width': defaultArticleState.contentWidth.value,
-					'--bg-color': defaultArticleState.backgroundColor.value,
+					'--font-family': articleState.fontFamilyOption.value,
+					'--font-size': articleState.fontSizeOption.value,
+					'--font-color': articleState.fontColor.value,
+					'--container-width': articleState.contentWidth.value,
+					'--bg-color': articleState.backgroundColor.value,
 				} as CSSProperties
 			}>
-			<ArticleParamsForm />
+			<ArticleParamsForm
+				state={formState}
+				setState={setFormState}
+				resetStyles={resetFormState}
+				applyStyles={applyState}
+			/>
 			<Article />
-		</div>
+		</main>
 	);
 };
 
+// Сборка приложения
 root.render(
 	<StrictMode>
 		<App />
